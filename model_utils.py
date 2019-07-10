1e963bd229fb6bd9591fe680c66ab02010c5eeb8
dorimedini <dorimedini@gmail.com>
2019-07-10 17:11:43 +0300

    Killed utils
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- model_utils.py
+++
@@ -1,7 +0,0 @@
-from keras import backend as K
-
-
-
-def reset_to_checkpoint(model, checkpoint_weights):
-    for idx in range(len(checkpoint_weights)):
-        model.layers[idx].set_weights(checkpoint_weights[idx])
