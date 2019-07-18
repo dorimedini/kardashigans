b67a92bfc471bcaae1d5d7af571a7e8e8ebf1f06
galshachaf <galshac@gmail.com>
2019-07-18 15:21:51 +0300

    calc_robustness works with array of layer weights instead of source model
    Signed-off-by: galshachaf <galshac@gmail.com>

--- analyze_model.py
+++ analyze_model.py
@@ -5,6 +5,8 @@ import seaborn as sns
 
 from keras import backend as K
 from kardashigans.verbose import Verbose
+from keras import Model
+
 
 class AnalyzeModel(object):
     """
@@ -38,6 +40,7 @@ class AnalyzeModel(object):
         :param model: The model to evaluate
         :param source_weights_model: The model from which we should
             copy weights and update our model's weights before eval.
+            also accepts list of layer weights.
         :param layer_indices: Layers to reset the weights of.
         :param batch_size: used in evaluation
         :return: A number in the interval [0,1] representing accuracy.
@@ -48,7 +51,10 @@ class AnalyzeModel(object):
         prev_weights = model.get_weights()
         if source_weights_model:
             for idx in layer_indices:
-                loaded_weights = source_weights_model.layers[idx].get_weights()
+                if isinstance(source_weights_model, Model):
+                    loaded_weights = source_weights_model.layers[idx].get_weights()
+                else:
+                    loaded_weights = source_weights_model[idx]
                 model.layers[idx].set_weights(loaded_weights)
         else:
             AnalyzeModel._rernd_layers(model, layer_indices)
