f86d9e75aa70eb54fa7afba55ce2578c472ca914
dorimedini <dorimedini@gmail.com>
2019-07-19 19:59:04 +0300

    Now the formatter is timezone-aware
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- verbose.py
+++ verbose.py
@@ -1,4 +1,6 @@
 import logging
+from datetime import datetime
+from pytz import timezone, utc
 
 
 LOG_LEVEL = logging.DEBUG
@@ -11,5 +13,15 @@ class Verbose(object):
         self._init_logging()
 
     def _init_logging(self):
+        logging.basicConfig(level=LOG_LEVEL,
+                            format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+                            datefmt="%Y-%m-%d %H:%M:%S")
+        logging.Formatter.converter = Verbose.custom_time
         self.logger = logging.getLogger(self._name)
-        self.logger.setLevel(level=LOG_LEVEL)
+
+    @staticmethod
+    def custom_time(*args):
+        utc_dt = utc.localize(datetime.utcnow())
+        my_tz = timezone("Israel")
+        converted = utc_dt.astimezone(my_tz)
+        return converted.timetuple()
